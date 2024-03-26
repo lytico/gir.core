7a6b9bc8ab15e9d7505af52950149ca780b5ba29
lytico <lytico@users.sourceforge.net>
2024-03-27 00:14:12 +0100

    introduce Samples.csproj from https://github.com/GtkSharp/GtkSharp/tree/develop/Source/Samples

---
+++ src/Samples/Samples/Sections/Widgets/ImageSection.cs
@@ -0,0 +1,30 @@
+﻿// This is free and unencumbered software released into the public domain.
+// Happy coding!!! - GtkSharp Team
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Gdk;
+using GdkPixbuf;
+using Gtk;
+
+namespace Samples
+{
+	[Section(ContentType = typeof(ImageBox), Category = Category.Widgets)]
+	class ImageSection : ListSection
+	{
+		public ImageSection()
+		{
+			AddItem(CreateContainer());
+		}
+
+		public (string, Widget) CreateContainer()
+		{
+			var image = new Pixbuf(typeof(ImageSection).Assembly, "Testpic");
+			var container = new ImageBox(image);
+
+			return ($"{nameof(ImageBox)}:", container);
+		}
+	}
+}
